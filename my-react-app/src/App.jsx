4020a71b90170e12bc38ec1a97eb956ffd970a52
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 21:47:05 +0200

    Imported Header component into App component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,12 @@
+import React from 'react'
+import Header from './Header'
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <div className='bg-[#f7f9f9]'>
+      <Header></Header>
+    </div>
   )
 }
 
