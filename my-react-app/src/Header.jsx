84293e930d08ebc252de1610bcff27c4b2c3ca6f
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 21:47:26 +0200

    Created Header with a title

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className='bg-[#6BA368]'>
+      <h1 className='text-3xl pl-10 py-1.5 text-white font-light'>Teori<span className='text-black font-semibold italic'>handboken~</span></h1>
+    </div>
+  )
+}
+
+export default Header
