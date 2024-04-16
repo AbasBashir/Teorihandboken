7b9f4669413ce1cdc384dd1bd720369b2e03133e
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 23:58:12 +0200

    Added content to render the subnavbar for HTMLCSS heading

---
+++ my-react-app/src/HTMLCSS.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function HTMLCSS() {
+    return (
+        <div className='flex-col space-y-2'>
+            <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>HTML & CSS</h4>
+            <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Responsiv design</h4>
+            <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Tillgänglighet</h4>
+            <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Aktuella webbstandarder</h4>
+            <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Pre-processorer</h4>
+            <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Optimering och validering</h4>
+        </div>
+      )
+}
+
+export default HTMLCSS
