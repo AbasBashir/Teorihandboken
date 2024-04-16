edd1f8aab0beaddfa761226d99959cf2aa6f060f
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 21:54:50 +0200

    Establishing a mockup for navbar

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className='bg-[#6BA368]'>
+    <div className='bg-[#6BA368] border-b-2 border-black'>
       <h1 className='text-3xl pl-10 py-1.5 text-white font-light'>Teori<span className='text-black font-semibold italic'>handboken~</span></h1>
+      <nav className='fixed bg-[#6BA368] h-full w-80 border-t-2 border-black'>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+        </ul>
+      </nav>
     </div>
   )
 }
