8e6afce02b9989bbfaaac00dd4f5ce61ab2bd08f
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 22:41:34 +0200

    Added some mockup text for newly created Projektmetodik component.

--- my-react-app/src/Projektmetodik.jsx
+++ my-react-app/src/Projektmetodik.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Projektmetodik() {
   return (
-    <div>
-      
+    <div className='border-2 border-red-500 flex-col space-y-2'>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 text-lg'>Chapter 1</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 text-lg'>Chapter 2</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 text-lg'>Chapter 3</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 text-lg'>Chapter 4</h4>
     </div>
   )
 }
