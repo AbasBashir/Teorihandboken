52d1eb51957598e6e40d443d74f1f3d3dd94cf5e
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 22:55:10 +0200

    Added an overall padding to the subheading

--- my-react-app/src/Projektmetodik.jsx
+++ my-react-app/src/Projektmetodik.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Projektmetodik() {
   return (
-    <div className='border-2 border-red-500 flex-col space-y-2'>
-        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 text-lg'>Chapter 1</h4>
-        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 text-lg'>Chapter 2</h4>
-        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 text-lg'>Chapter 3</h4>
-        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 text-lg'>Chapter 4</h4>
+    <div className='flex-col space-y-2'>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Agila metoder</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Icke-agila metoder</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Entreprenörskap</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Issue distribution & Handling</h4>
     </div>
   )
 }
