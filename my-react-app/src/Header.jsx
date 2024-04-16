66564f9afac33a9304fa867abeb5229e6d5ade5a
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 22:03:41 +0200

    Added the main titles for the teorihandboken

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -2,22 +2,20 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className='bg-[#6BA368] border-b-2 border-black'>
+    <div className='bg-[#6BA368] border-b-[1.5px]'>
       <h1 className='text-3xl pl-10 py-1.5 text-white font-light'>Teori<span className='text-black font-semibold italic'>handboken~</span></h1>
-      <nav className='fixed bg-[#6BA368] h-full w-80 border-t-2 border-black'>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-            <li>7</li>
-            <li>8</li>
-            <li>9</li>
+      <nav className='fixed bg-[#6BA368] h-full w-80 border-t-[1.5px] border-[#98c897]'>
+        <ul className='relative top-5 flex flex-col space-y-12 justify-center border-2 border-red-500 p-3'>
+            <li>Projektmetodik</li>
+            <li>HTML-CSS</li>
+            <li>UX-design</li>
+            <li>Programmeringsmetodik</li>
+            <li>JavaScript</li>
+            <li>Backendutveckling</li>
+            <li>Avancerad-JavaScript</li>
         </ul>
       </nav>
-    </div>
+    </div>  
   )
 }
 
