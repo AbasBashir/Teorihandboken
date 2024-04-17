b392c645f238d8f724f79d128aab149e6c0e52ad
AbasBashir <MrAbasBashir@gmail.com>
2024-04-17 18:32:09 +0200

    Added the text intro to the subheading.

--- my-react-app/src/HTMLCSS.jsx
+++ my-react-app/src/HTMLCSS.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function HTMLCSS() {
     return (
         <div className='flex-col space-y-2'>
-            <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>HTML & CSS</h4>
+            <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Intro into HTML & CSS</h4>
             <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Responsiv design</h4>
             <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Tillgänglighet</h4>
             <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Aktuella webbstandarder</h4>
