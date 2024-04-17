cd645439cd2720750a08080bd551c158d0c66f34
AbasBashir <MrAbasBashir@gmail.com>
2024-04-17 18:04:44 +0200

    Created a Programmeringsmetodik component and added all subnavbar links

---
+++ my-react-app/src/Programmeringsmetodik.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Programmeringsmetodik() {
+  return (
+    <div className='flex-col space-y-2'>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Versionshantering</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Benchmarking</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Testdriven utveckling</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Deploy och staging</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Debugging</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Dokumentation</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Struktur av kod</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Automatisering av arbetsflöde</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Virtualisering av utvecklingsmiljö</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Bundeling-verktyg</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Terminalinterface</h4>
+    </div>
+  )
+}
+
+export default Programmeringsmetodik
