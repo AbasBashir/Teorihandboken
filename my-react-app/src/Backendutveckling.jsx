28c858858656bd3c53573b46c5b954fb08c49645
AbasBashir <MrAbasBashir@gmail.com>
2024-04-17 18:19:38 +0200

    Created a Backendutveckling component containing the subnavbar links.

---
+++ my-react-app/src/Backendutveckling.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Backendutveckling() {
+  return (
+    <div className='flex-col space-y-2'>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>PHP</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>OOP i PHP</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Säkerhet i PHP</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>MVC</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Wordpress</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Heirarkiska databaser</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Relationsdatabaser, SQL och ER-modellering</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>OAuth i backend</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>HTTP-protokollet</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>cURL</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>REST</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>XML och andra dataformat</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Webbservrar</h4>
+    </div>
+  )
+}
+
+export default Backendutveckling
