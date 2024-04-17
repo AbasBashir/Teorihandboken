bfe48ebfa663c17abbecabb1a6029764db217c16
AbasBashir <MrAbasBashir@gmail.com>
2024-04-17 18:18:59 +0200

    Established a better indentation.

--- my-react-app/JavaScript.jsx
+++ my-react-app/JavaScript.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 function JavaScript() {
   return (
     <div className='flex-col space-y-2'>
-    <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>ECMAScript</h4>
-    <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Ramverk och Bibliotek</h4>
-    <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Promises</h4>
-    <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>OOP i JavaScript</h4>
-    <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>DOM-manipulation</h4>
-    <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>HTTP-requests</h4>
-    <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Lexical scope</h4>
-    <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Event handling</h4>
-    <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Prototype inheritance</h4>
-    <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Higher-order functions</h4>
-    <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Single-thread programming</h4>
-    <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>OAuth från frontend</h4>
-    <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Websockets</h4>
-</div>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>ECMAScript</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Ramverk och Bibliotek</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Promises</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>OOP i JavaScript</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>DOM-manipulation</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>HTTP-requests</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Lexical scope</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Event handling</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Prototype inheritance</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Higher-order functions</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Single-thread programming</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>OAuth från frontend</h4>
+        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Websockets</h4>
+    </div>
   )
 }
 
