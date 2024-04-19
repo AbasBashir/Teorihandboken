2f21e7e9b57ff4f1867116e72ffa1575bc33007e
AbasBashir <MrAbasBashir@gmail.com>
2024-04-19 20:14:43 +0200

    Optimized the code by converting few lines of code into a single line of code.

--- my-react-app/src/Projektmetodik.jsx
+++ my-react-app/src/Projektmetodik.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Link} from 'react-router-dom'
 
 function Projektmetodik() {
+
+  const heading = ['Agila metoder','Icke-agila metoder','Entreprenorskap','Issue distribution & Handling'];
+
   return (
     <div className='flex-col space-y-2'>
-        <Link to={'/Projektmetodik/AgilaMetoder'}><h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Agila metoder</h4></Link>
-        <Link to={'/Projektmetodik/IckeAgilaMetoder'}><h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Icke-agila metoder</h4></Link>
-        <Link to={'/Projektmetodik/Entreprenorskap'}><h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Entreprenörskap</h4></Link>
-        <Link to={'/Projektmetodik/IssuedistributionAndHandling'}><h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Issue distribution & Handling</h4></Link>
+      {heading.map((mainHeading,index) => (<Link to={`/Projektmetodik/${mainHeading}`} key={index}><h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>{mainHeading}</h4></Link>))}
     </div>
   )
 }
