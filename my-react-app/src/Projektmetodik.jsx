122d57a93b877292268119ed1fcba4c5ed92afef
AbasBashir <MrAbasBashir@gmail.com>
2024-04-19 18:00:59 +0200

    Imported Link component and added it to all headings

--- my-react-app/src/Projektmetodik.jsx
+++ my-react-app/src/Projektmetodik.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link} from 'react-router-dom'
 
 function Projektmetodik() {
   return (
     <div className='flex-col space-y-2'>
-        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Agila metoder</h4>
-        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Icke-agila metoder</h4>
-        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Entreprenörskap</h4>
-        <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Issue distribution & Handling</h4>
+        <Link to={'/Projektmetodik/AgilaMetoder'}><h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Agila metoder</h4></Link>
+        <Link to={'/Projektmetodik/IckeAgilaMetoder'}><h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Icke-agila metoder</h4></Link>
+        <Link to={'/Projektmetodik/Entreprenorskap'}><h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Entreprenörskap</h4></Link>
+        <Link to={'/Projektmetodik/IssuedistributionAndHandling'}><h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Issue distribution & Handling</h4></Link>
     </div>
   )
 }
