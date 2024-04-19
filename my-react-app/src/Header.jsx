991a8874b5ef2685675fb79b3c7fcc7c681972b4
AbasBashir <MrAbasBashir@gmail.com>
2024-04-19 18:10:15 +0200

    Added border color

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 function Header() {
 
     return (
-        <div className='bg-[#6BA368] border-b-[1.5px]'>
+        <div className='bg-[#6BA368] border-b-[1.5px] border-[#98c897]'>
             <h1 className='text-3xl pl-10 py-1.5 text-white font-light'>Teori<span className='text-black font-semibold italic'>handboken~</span></h1>
             <Navbar></Navbar>
         </div>  
