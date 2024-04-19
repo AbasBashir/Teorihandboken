195ae92375210845f38fb85a80ffbb1a1fb428d4
AbasBashir <MrAbasBashir@gmail.com>
2024-04-19 18:11:30 +0200

    Established a router set-up for one of the core theories and also included RouterProvider and the router attribute.

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,15 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {createBrowserRouter} from 'react-router-dom';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import AgilaMetoder from './ReadProjektmetodik/AgilaMetoder.jsx';
+import IckeAgilaMetoder from './ReadProjektmetodik/IckeAgilaMetoder.jsx';
 
 const router = createBrowserRouter([
-  
+  {
+    path: '/',
+    element: <App/>,
+    children:[
+      {
+        path: 'Projektmetodik/AgilaMetoder',
+        element: <AgilaMetoder/>,
+      },
+      {
+        path: 'Projektmetodik/IckeAgilaMetoder',
+        element: <IckeAgilaMetoder/>,
+      },
+      {
+        path: 'Projektmetodik/Entreprenorskap',
+        element: <IckeAgilaMetoder/>,
+      },
+      {
+        path: 'Projektmetodik/IssuedistributionAndHandling',
+        element: <IckeAgilaMetoder/>,
+      },
+    ]
+  },
+
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+    <RouterProvider router={router}>
+
+    </RouterProvider>
+
   </React.StrictMode>,
 )
 
