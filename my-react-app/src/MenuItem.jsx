ab1e965ebae9cb2b6df6d19c919885fbf2034665
AbasBashir <MrAbasBashir@gmail.com>
2024-04-19 20:17:29 +0200

    Divided the Navbar component into two component one of them being the MenuItem component which essentially presents the content.

---
+++ my-react-app/src/MenuItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import menuDown from './assets/menu-down.svg';
+
+function MenuItem({ title, content, isVisible, toggleSubnav }) {
+  
+  return (
+    <div>
+        <div className='flex items-center justify-between hover:bg-black hover:text-white' onClick={toggleSubnav}>
+            <li className='hover:text-white'><button className='inline-block w-full p-2 text-lg font-semibold font-mono'>{title}</button></li>
+            <img src={menuDown} alt="menu icon" className="w-8 h-8 mr-4 cursor-pointer hover:text-black" />
+        </div>
+        <div className={`transition-all duration-100 ${isVisible ? 'opacity-100 scale-y-10' : 'opacity-0 scale-y-0 h-0'}`}>
+            {content}
+        </div>
+    </div>
+  )
+}
+
+export default MenuItem
