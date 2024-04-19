51b20eff7ed661bbeb34f0cd0b7d083aecf3b7b1
AbasBashir <MrAbasBashir@gmail.com>
2024-04-19 18:06:56 +0200

    Created components for each subheading and added some intial content. This will later be replaced.

---
+++ my-react-app/src/ReadProjektmetodik/AgilaMetoder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AgilaMetoder() {
+  return (
+    <div>
+      <h1 className='text-center bg-red-500'>This is the content for Agila metoder in Projektmetodik</h1>
+    </div>
+  )
+}
+
+export default AgilaMetoder
---
+++ my-react-app/src/ReadProjektmetodik/Entreprenorskap.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Entreprenorskap() {
+  return (
+    <div>
+        <h1 className='text-center bg-blue-500'>This is the content for Agila metoder in Projektmetodik</h1>
+    </div>
+  )
+}
+
+export default Entreprenorskap
