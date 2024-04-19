4f6b3dc1f125ce3c55d9f1e7bfe5b7d42983e4a3
AbasBashir <MrAbasBashir@gmail.com>
2024-04-19 18:07:52 +0200

    Added Outlet component to App component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Header from './Header'
+import { Outlet } from 'react-router-dom'
+
 
 function App() {
 
   return (
-    <div className='bg-[#f7f9f9]'>
+    <div className='bg-[#f9f7f7]'>
       <Header></Header>
+      <Outlet/>
     </div>
   )
 }
 
 export default App
+
+
+
+
+
