ea744096363a3972eea8511f0a8f34ae60543850
AbasBashir <MrAbasBashir@gmail.com>
2024-04-20 11:39:18 +0200

    Implemented the useContext hook in order to pass on a function that will set navbarStatus to false when a link is clicked and thus close the navbar.

--- my-react-app/src/Projektmetodik.jsx
+++ my-react-app/src/Projektmetodik.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { NavbarContext } from './Header';
 import { NavLink} from 'react-router-dom'
 
 function Projektmetodik() {
 
   const heading = ['Agila metoder','Icke-agila metoder','Entreprenorskap','Issue distribution & Handling'];
+
+  const closeNavbar = useContext(NavbarContext);
 
   return (
     <div className='flex-col space-y-2'>
@@ -13,7 +16,7 @@ function Projektmetodik() {
         return {
           color: isActive ? "#fff" : "",
         };
-      }} to={`/Projektmetodik/${mainHeading}`} key={index}>
+      }} to={`/Projektmetodik/${mainHeading}`} key={index} onClick={closeNavbar}>
         <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>{mainHeading}</h4>
       </NavLink>))}
 
