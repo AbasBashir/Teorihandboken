ebabc4a9a4223eefc41fcd46e866bf7ad2543317
AbasBashir <MrAbasBashir@gmail.com>
2024-04-20 18:08:26 +0200

    Moved the content and functionality for Nav component to the App component.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React,{useState, createContext} from "react";
 import Header from './Header'
+import Navbar from './Navbar';
 import { Outlet } from 'react-router-dom'
 
+export const NavbarContext = createContext();
 
 function App() {
 
+  const [navbarStatus, setNavbarStatus] = useState(false); // State and handler for navbar
+
+  const handleNavbar = () => {
+    setNavbarStatus(!navbarStatus);
+    console.log(navbarStatus);
+  };
+
+  const closeNavbar = () => {
+    setNavbarStatus(false);
+  };
+
   return (
     <div className='bg-[#f9f7f7]'>
-      <Header></Header>
-      <Outlet/>
+      <Header handleNavbar={handleNavbar} navbarStatus={navbarStatus}></Header>
+      <div className="flex flex-col sm:flex-row">
+        <NavbarContext.Provider value={closeNavbar}>
+            <Navbar navbarStatus={navbarStatus}/>
+        </NavbarContext.Provider>
+        <Outlet/>
+      </div>
     </div>
   )
 }
--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -1,37 +1,17 @@
-import React,{useState, createContext} from "react";
-import Navbar from "./Navbar";
 import { SquareArrowLeft, SquareArrowRight } from "lucide-react";
 
-export const NavbarContext = createContext();
 
-function Header() {
-
-    const [navbarStatus, setNavbarStatus] = useState(false);
-
-    const handleNavbar = () =>{
-        setNavbarStatus(!navbarStatus) ;
-    }
-
-    const closeNavbar = () => {
-        setNavbarStatus(false);
-    };
+function Header({ handleNavbar, navbarStatus }) {
 
     return (
-        <NavbarContext.Provider value={closeNavbar}>
-            <div className='bg-[#6BA368] border-b-[1.5px] border-[#98c897]'>
-                <div className=" sm:w-fit flex items-center">
-                    <button className="ml-4 relative top-[4px] cursor-pointer sm:hidden" onClick={handleNavbar}>
-                        {navbarStatus ? <SquareArrowRight className="w-7 h-7 "/> : <SquareArrowLeft className="w-7 h-7"/>}
-                    </button >
-                    <h1 className='text-3xl pl-1 py-1.5 text-white font-light sm:pl-10 w-full text-center sm:text-left'>Teori<span className='text-black font-semibold italic'>handboken~</span></h1>
-                </div>
-                
-            <div className={`transition-all duration-500 ease-in-out ${navbarStatus ? 'translate-x-0' : '-translate-x-full sm:translate-x-0 w-80'}`}>
-                    <Navbar />
-                </div>
-                
-            </div>  
-        </NavbarContext.Provider>
+        <div className='bg-[#6BA368] border-b-[1.5px] border-[#98c897]'>
+            <div className=" sm:w-fit flex items-center">
+                <button className="ml-4 relative top-[4px] cursor-pointer sm:hidden" onClick={handleNavbar}>
+                    {navbarStatus ? <SquareArrowRight className="w-7 h-7 "/> : <SquareArrowLeft className="w-7 h-7"/>}
+                </button >
+                <h1 className='text-3xl pl-1 py-1.5 text-white font-light sm:pl-10 w-full text-center sm:text-left'>Teori<span className='text-black font-semibold italic'>handboken~</span></h1>
+            </div>
+        </div>  
     )
 }
 
