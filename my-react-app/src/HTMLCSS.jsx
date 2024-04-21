17c6437b376a0c018e03bb136c5b67d0ef173cd1
AbasBashir <MrAbasBashir@gmail.com>
2024-04-21 14:44:53 +0200

    Optimized my navbarlinks.

--- my-react-app/src/HTMLCSS.jsx
+++ my-react-app/src/HTMLCSS.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { NavbarContext } from './App';
+import { NavLink} from 'react-router-dom'
 
 function HTMLCSS() {
+    const heading = ['HTML & CSS','Responsiv design','Tillganglighet inom webb','Aktuella webbstandarder', 'CSS Pre-processorer', 'Optimering och validering av HTML & CSS'];
+
+    const closeNavbar = useContext(NavbarContext);
+  
     return (
-        <div className='flex-col space-y-2'>
-            <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Intro into HTML & CSS</h4>
-            <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Responsiv design</h4>
-            <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Tillgänglighet</h4>
-            <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Aktuella webbstandarder</h4>
-            <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Pre-processorer</h4>
-            <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>Optimering och validering</h4>
-        </div>
-      )
+      <div className='flex-col space-y-2'>
+        {heading.map((mainHeading,index) => (
+  
+        <NavLink style={({ isActive}) => {
+          return {
+            color: isActive ? "#fff" : "",
+          };
+        }} to={`/HTMLCSS/${mainHeading}`} key={index} onClick={closeNavbar}>
+          <h4 className='cursor-pointer hover:bg-black hover:text-white pl-4 p-1 text-md'>{mainHeading}</h4>
+        </NavLink>))}
+  
+      </div>
+    )
 }
 
 export default HTMLCSS
