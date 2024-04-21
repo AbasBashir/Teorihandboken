7feaa63890ec4557c36004feee1fba2204dd164a
AbasBashir <MrAbasBashir@gmail.com>
2024-04-21 14:45:31 +0200

    Added new routes to my next heading.

--- my-react-app/src/routes.jsx
+++ my-react-app/src/routes.jsx
@@ -6,6 +6,12 @@ import IckeAgilaMetoder from './ReadProjektmetodik/IckeAgilaMetoder.jsx';
 import Entreprenorskap from './ReadProjektmetodik/Entreprenorskap.jsx';
 import IssuedistributionAndHandling from './ReadProjektmetodik/IssuedistributionAndHandling.jsx';
 import PageNotFound from './PageNotFound.jsx';
+import HTMLCSS from './ReadHTML-CSS/HTMLCSS.jsx';
+import Optimering from './ReadHTML-CSS/Optimering.jsx';
+import Preprocessorer from './ReadHTML-CSS/Preprocessorer.jsx';
+import ResponsivDesign from './ReadHTML-CSS/ResponsivDesign.jsx';
+import Tillganglighet from './ReadHTML-CSS/Tillganglighet.jsx';
+import Webbstandarder from './ReadHTML-CSS/Webbstandarder.jsx';
 
 const router = createBrowserRouter([
   {
@@ -28,6 +34,29 @@ const router = createBrowserRouter([
       {
         path: 'Projektmetodik/Issue distribution & Handling',
         element: <IssuedistributionAndHandling/>,
+      },{
+        path: 'HTMLCSS/HTML & CSS',
+        element: <HTMLCSS/>,
+      },
+      {
+        path: 'HTMLCSS/Responsiv design',
+        element: <ResponsivDesign/>,
+      },
+      {
+        path: 'HTMLCSS/Tillganglighet inom webb',
+        element: <Tillganglighet/>,
+      },
+      {
+        path: 'HTMLCSS/Aktuella webbstandarder',
+        element: <Webbstandarder/>,
+      },
+      {
+        path: 'HTMLCSS/CSS Pre-processorer',
+        element: <Preprocessorer/>,
+      },
+      {
+        path: 'HTMLCSS/Optimering och validering av HTML & CSS',
+        element: <Optimering/>,
       },
     ]
   },
