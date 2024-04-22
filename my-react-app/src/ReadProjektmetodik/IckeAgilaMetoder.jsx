e3e93880774cf13ba82f1758a19aa86465ad3280
AbasBashir <MrAbasBashir@gmail.com>
2024-04-22 16:06:37 +0200

    Removed a line break

--- my-react-app/src/ReadProjektmetodik/IckeAgilaMetoder.jsx
+++ my-react-app/src/ReadProjektmetodik/IckeAgilaMetoder.jsx
@@ -16,7 +16,7 @@ function IckeAgilaMetoder() {
   </p>  
   <br><br>
   The first stage of the waterfall approach is called requirements. Here, the specifications, planning phase and the goal of the project are acknowledged. The project manager then communicates with project sponsors and identifies relevant information such the risks, assumptions, dependencies, quality metrics, costs, timeline etc. Next, the design phase is where the details and documentation regarding the project are crystallized and approved. This stage is also where the project’s schedule, objectives, budgets and essentially the blueprint of the project is designed like using Figma. Next phase is the implementation or coding phase. This is where a software developer most of the time is spent on this phase, where the software for the project is being built based on the design. Next up is the testing phase and this is where the software is being tested based on different quality metrics and customer satisfaction and that it fulfills all the requirements established. If all requirements have been met and the software is working, it is ready to be deployed and launched to the user or customers. The last stage is called maintenance, and this is where minor parts are modified to improve the software, identify errors, and perform general maintenance of the software.  
-  <br><br>
+  <br>
   # **Sources**: 
 
 1. [chasacademy.instructure.com](https://chasacademy.instructure.com/)
