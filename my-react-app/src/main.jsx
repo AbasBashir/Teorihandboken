ba8ec9493e454ea91e633df89a426cd964b26f8a
AbasBashir <MrAbasBashir@gmail.com>
2024-04-22 15:44:22 +0200

    Moved the routes to Routes directory.

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {RouterProvider} from 'react-router-dom';
 import './index.css'
-import router from './routes.jsx'
+import router from './Routes/routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
