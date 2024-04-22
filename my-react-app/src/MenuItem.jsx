5d5fe8016a13f642402ff6b23337ef6924cc4c30
AbasBashir <MrAbasBashir@gmail.com>
2024-04-22 15:46:27 +0200

    Added useState() hook and special event handlers that will aid in setting the filter property on and off in order to display the menuDown icon.

--- my-react-app/src/MenuItem.jsx
+++ my-react-app/src/MenuItem.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
 import menuDown from './assets/menu-down.svg';
 
 function MenuItem({ title, content, isVisible, toggleSubnav, index }) {
   
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div>
-        <div className='flex items-center justify-between hover:bg-black hover:text-white' onClick={toggleSubnav}>
+        <div className='flex items-center justify-between hover:bg-black hover:text-white'  onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} onClick={toggleSubnav}>
             <li className='hover:text-white'><button className='inline-block w-full pl-5 p-2 text-lg font-semibold font-mono'>{index+1}.{title}</button></li>
-            <img src={menuDown} alt="menu icon" className="w-8 h-8 mr-4 cursor-pointer hover:text-black" />
+            <img src={menuDown} alt="menu icon" className="w-8 h-8 mr-4 cursor-pointer" style={{ filter: isHovered ? 'none' : 'invert(1)' }}/>
         </div>
-        <div className={`transition-all duration-100 pl-5 ${isVisible ? 'opacity-100 scale-y-10' : 'opacity-0 scale-y-0 h-0'}`}>
+        <div className={`transition-all duration-700 pl-5 overflow-hidden ${isVisible ? 'h-auto max-h-96' : 'max-h-0'}`}>
             {content}
         </div>
     </div>
