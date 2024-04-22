bda50afa565cb373a89451fb2814907d98a7716b
AbasBashir <MrAbasBashir@gmail.com>
2024-04-22 15:43:34 +0200

    Added new brandLogo, styled the header whereby I added padding and stroke.

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -1,15 +1,17 @@
 import { SquareArrowLeft, SquareArrowRight } from "lucide-react";
-
+import BrandLogo from '../../assets/BrandLogo.svg'
 
 function Header({ handleNavbar, navbarStatus }) {
 
     return (
-        <div className='sticky top-0 sm:relative bg-[#6BA368] border-b-[1.5px] border-[#98c897]'>
+        <div className='sticky top-0 sm:relative bg-[#ffffff] border-b-[3px] border-[#000000] shadow-md'>
             <div className=" sm:w-fit flex items-center">
-                <button className="ml-4 relative top-[4px] cursor-pointer sm:hidden" onClick={handleNavbar}>
-                    {navbarStatus ? <SquareArrowRight className="w-7 h-7 "/> : <SquareArrowLeft className="w-7 h-7"/>}
+                <button className="ml-4 relative top-[1.2px] cursor-pointer sm:hidden" onClick={handleNavbar}>
+                    {navbarStatus ? <SquareArrowRight className="w-7 h-7 " style={{ stroke: '#000000' }}/> : <SquareArrowLeft className="w-7 h-7" style={{ stroke: '#000000' }}/>}
                 </button >
-                <h1 className='text-3xl pl-1 py-1.5 text-white font-light sm:pl-10 w-full text-center sm:text-left'>Teori<span className='text-black font-semibold italic'>handboken~</span></h1>
+                <span className="w-full py-3 ml-5 sm:ml-0 sm:pl-5">
+                    <img src={BrandLogo} alt="" className="w-15 h-8 mx-auto sm:mx-none sm:h-9"/>
+                </span>
             </div>
         </div>  
     )
