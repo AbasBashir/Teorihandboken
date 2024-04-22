ff877f96f4f7f7684e85efd27ca90569b7277e34
AbasBashir <MrAbasBashir@gmail.com>
2024-04-22 15:51:53 +0200

    Created component that will be rendered in App component and added some data data.

---
+++ my-react-app/src/NavLinks/AvanceradJavaScript.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AvanceradJavaScript() {
+  return (
+    <div>
+        <h1 className='pl-5 font-bold'>COMING SOON....</h1>
+    </div>
+  )
+}
+
+export default AvanceradJavaScript
